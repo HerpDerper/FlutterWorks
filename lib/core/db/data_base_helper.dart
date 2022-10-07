4251ae169602055768dff27f54b8f9e714cf1f21
HerpDerp <eduardkutermin@gmail.com>
2022-10-07 14:08:21 +0300

    for nikita

---
+++ lib/core/db/data_base_helper.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:flutter_application_1/common/data_base_request.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+
+class DataBaseHelper {
+  static final DataBaseHelper instance = DataBaseHelper._instance();
+
+  DataBaseHelper._instance();
+
+  late final Directory _appDocumentDirectory;
+  late final String _pathDB;
+  late final Database dataBase;
+  int _version = 1;
+
+  Future<void> init() async {
+    _appDocumentDirectory =
+        await path_provider.getApplicationDocumentsDirectory();
+
+    _pathDB = join(_appDocumentDirectory.path, 'booksstore.db');
+
+    if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
+    } else {
+      dataBase = await openDatabase(_pathDB,
+          version: _version, onCreate: (db, version) {});
+    }
+  }
+
+  Future<void> onCreateTable(Database db) async {
+    for (var i = 0; i < DataBaseRequest.tableList.length; i++) {
+      await db.execute(DataBaseRequest.createTableList[i]);
+    }
+  }
+}
